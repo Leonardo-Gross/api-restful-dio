2d2176028581dab6ff787e6b5003f56009446261
Leonardo <leogrossms@gmail.com>
2024-07-28 09:01:28 -0300

    First Commit

---
+++ src/main/java/me/dio/api_rest_santander/ApiRestSantanderApplication.java
@@ -0,0 +1,13 @@
+package me.dio.api_rest_santander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSantanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/api_rest_santander/ApiRestSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.api_rest_santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
